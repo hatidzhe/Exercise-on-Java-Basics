6db40e8e5bd09940f962734bd62604c420099cf1
Hatidzhe <yusufova.h@gmail.com>
2015-07-10 15:35:05 +0300

    chg: Exercise 1 added

---
+++ src/com/hatidzhe/Main.java
@@ -0,0 +1,14 @@
+package com.hatidzhe;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int mark = 49;
+    System.out.println("The mark is " +  mark);
+        if (mark >= 50) {
+            System.out.println("PASS");}
+        else {
+            System.out.println("FAIL");
+        }
+    }
+}
