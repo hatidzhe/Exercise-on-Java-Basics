2181886dede118e08d09d97c1c63823011559393
Hatidzhe <yusufova.h@gmail.com>
2015-07-11 13:48:11 +0300

    chg: 2nd exercise added(even or odd)

--- src/com/hatidzhe/Main.java
+++ src/com/hatidzhe/Main.java
@@ -3,21 +3,17 @@ package com.hatidzhe;
 public class Main {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
+
     int number = 49;
         System.out.println("The number is " + number);
 
         if (number % 2 == 0){ System.out.println("The number is even");
 
-    } else { System.out.println("The number is odd");}}
-=======
-    int mark = 49;
-    System.out.println("The mark is " +  mark);
-        if (mark >= 50) {
-            System.out.println("PASS");}
-        else {
-            System.out.println("FAIL");
+    } else { System.out.println("The number is odd");
         }
+    
+  
+  
     }
->>>>>>> 6db40e8e5bd09940f962734bd62604c420099cf1
+
 }
